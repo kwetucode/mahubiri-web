286fc6ceb9ac407a315adf61737bbe68bf30fcea
mkbcentral <mkbcentralcode@gmail.com>
2025-10-14 15:32:49 +0200

    Work for fix route api

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome')->with('version', app()->version());
-});
+    return view('welcome')->with('version', app()
+        ->version());
+})->name('welcome');
